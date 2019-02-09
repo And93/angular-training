ee74fcc357e5038bdb9c6cccef947bfb01b185d3
Andrei Dzeichyk <andreidei4ik@gmail.com>
2019-02-09 18:32:10 +0300

    Implemented:
    - function for getting random string with necessary length;
    - config service;
    - localstorage service.

---
+++ src/app/common/config-options/config-options.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {ConfigOptions} from './config-options.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigOptionsService {
+
+  private settings: ConfigOptions;
+
+  constructor() {
+  }
+
+  setSettings(params: any) {
+    return this.settings = params;
+  }
+
+  getSettings() {
+    return this.settings;
+  }
+}
---
+++ src/app/common/random-generator/random-generator.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+
+const letters = 'abcdefghijklmnopqrstuvwxyz';
+const numbs = '0123456789';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RandomGeneratorService {
+
+  constructor() {
+  }
+
+  getRandomString(n: number) {
+    let values = '';
+
+    const possible = letters + letters.toUpperCase() + numbs;
+
+    for (let i = 0; i < n; i++) {
+      values += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return values;
+  }
+
+}
